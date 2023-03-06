670093ffefcf346b4a76af82611f2ad1c5c06dfa
rafinhabusatta <lacerdabusatta@gmail.com>
2023-03-06 13:08:54 -0300

    Criação do componente Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Button } from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Button label="Click me" onClick={() => setCount((count) => count + 1)} />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
