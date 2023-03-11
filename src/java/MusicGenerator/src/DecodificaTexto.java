1f1debb1beb613daec8516128c94a1a9908e6746
tsa3 <tsa3@cin.ufpe.br>
2023-03-11 13:42:57 -0300

    feat: add DecodificaTexto class

---
+++ src/java/MusicGenerator/src/DecodificaTexto.java
@@ -0,0 +1,24 @@
+public class DecodificaTexto {
+    private String textoMusical;
+
+    public DecodificaTexto(String textoMusical) {
+        this.textoMusical = textoMusical;
+    }
+
+    public DecodificaTexto() {
+        this.textoMusical = "";
+    }
+
+    public String getTextoMusical() {
+        return textoMusical;
+    }
+
+    public void setTextoMusical(String textoMusical) {
+        this.textoMusical = textoMusical;
+    }
+
+    public String processaTexto(){
+
+        return "";
+    }
+}
