6ac3998012e1c363e0a74d1b15e08a8eee678e15
rsbarbosinha <rsbarbosinha@hotmail.com>
2023-03-13 13:46:18 -0300

    Criacao da classe TextReader

--- src/java/MusicGenerator/src/TextReader.java
+++ src/java/MusicGenerator/src/TextReader.java
@@ -1,2 +1,15 @@
-public class TextReader {
+import	java.io.File;
+import	java.io.FileNotFoundException;
+import	java.util.Scanner;
+
+public class TextReader	{
+	public static void main(String[] args) throws FileNotFoundException	{
+		//File file = new File("C:\Users\rsb_\TCP\TCP-ProjetoFinal\src\java\MusicGenerator\src\textodeteste.txt");
+		File file = new File("teste.txt");
+		Scanner scan = new Scanner(file);
+		while(scan.hasNextLine()){
+			System.out.println(scan.nextLine());
+			}
+			scan.close();
+		}
 }
