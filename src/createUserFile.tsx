d4c540b711a2d680fa231c99a875491b874993d9
rafinhabusatta <lacerdabusatta@gmail.com>
2023-03-15 12:23:10 -0300

    Finalização da função createUserFile, que agora salva o arquivo soundcraft.txt com o conteudo do textarea na pasta text dentro do src do projeto, automaticamente ao clicar no botão para gerar música

--- src/createUserFile.tsx
+++ src/createUserFile.tsx
@@ -1,13 +1,24 @@
-export function createUserFile( id: string) {
-  const text = (document.getElementById(id ) as HTMLTextAreaElement)?.value;
-  if (text) {
-    const blob = new Blob([text], { type: "text/plain" });
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = "soundcraft.txt";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    alert("Arquivo criado com sucesso!");
+export function createUserFile(id: string) {
+  const fs = require('fs')
+  const directory =  `${window.process.cwd()}/src/text`
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true })
   }
-}
+
+  const path = require('path');
+  const fileName = 'soundcraft.txt'
+  const defaultPath = path.join(directory, fileName);
+  
+  const fileContent = (document.getElementById(id)as HTMLTextAreaElement)?.value
+  fs.writeFile(defaultPath, fileContent, (err: String) => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('Arquivo salvo com sucesso em', defaultPath)
+    }
+  })
+  }
+
+
+
+
