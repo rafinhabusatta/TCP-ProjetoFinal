2689f3dbeb254d5fb87354483e9e295066eda914
Jgorosa <lermjv@gmail.com>
2023-03-22 19:02:21 -0300

    Update ControlaMusica.java
    Controla música agora salva o que foi tocado para um arquivo midi.

---
+++ src/java/MusicGenerator/src/Musica/ControlaMusica.java
@@ -0,0 +1,17 @@
+package Musica;
+
+import org.jfugue.player.Player;
+import org.jfugue.pattern.Pattern;
+
+
+public class ControlaMusica implements IControlaMusica {
+    public  Pattern TocaMusica(int bpm,int instrumento, String musicString) {
+
+        Pattern pattern = new Pattern("T" + bpm + " I" + instrumento + " " + musicString); // Cria o pattern com a velocidade e instrumento definidos e adiciona a sequência de notas
+        Player player = new Player(); // Cria o objeto Player do JFugue para tocar o pattern
+        player.play(pattern); // Toca o pattern usando o objeto Player
+
+        return pattern;
+
+    }
+}
---
+++ src/java/MusicGenerator/src/Musica/IControlaMusica.java
@@ -0,0 +1,8 @@
+package Musica;
+
+import org.jfugue.pattern.Pattern;
+
+public interface IControlaMusica {
+
+    Pattern TocaMusica(int bpm,int instrumento, String musicString);
+}
